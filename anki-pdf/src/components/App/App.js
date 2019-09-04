a370db84ca903a08562b048e20ade0da33ca76b0
derMacon <silash169@gmail.com>
2019-09-04 23:26:03 +0200

    Managed to add keymap to Hotkeys

--- anki-pdf/src/components/App/App.js
+++ anki-pdf/src/components/App/App.js
@@ -3,50 +3,26 @@ import {Menu} from '../Menu/Menu';
 import {MainWindow} from '../MainWindow/MainWindow';
 import './App.css';
 
-import Hotkeys from 'react-hot-keys';
+import { HotKeys } from "react-hotkeys";
+
+const keyMap = {
+  MOVE_UP: "up"
+};
+
+const handlers = {
+  MOVE_UP: event => console.log("Move up hotkey called!")
+};
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      output: 'Hello, I am a component that listens to keydown and keyup of a',
-    }
-  }
-  onKeyUp(keyName, e, handle) {
-    console.log("test:onKeyUp", e, handle)
-    this.setState({
-      output: `onKeyUp ${keyName}`,
-    });
-  }
-  onKeyDown(keyName, e, handle) {
-    console.log("test:onKeyDown", keyName, e, handle)
-    this.setState({
-      output: `onKeyDown ${keyName}`,
-    });
-  }
-  render() {
-    return (
-      <Hotkeys
-        keyName="shift+a,alt+s"
-        onKeyDown={this.onKeyDown.bind(this)}
-        onKeyUp={this.onKeyUp.bind(this)}
-      >
-        <div style={{ padding: "50px" }}>
-          {this.state.output}
-        </div>
-      </Hotkeys>
-    )
-  }
-}
 
-/*
   render() {
     return (
-      <div className="App">
-        <Menu/>
-        <MainWindow/>
-      </div>
+      <HotKeys keyMap={keyMap} handlers={handlers}>
+        <div className="App">
+          <Menu/>
+          <MainWindow/>
+        </div>
+      </HotKeys>
     );
   }
 }
-*/
