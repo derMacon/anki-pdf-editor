753cebdd23d6bacb20c195657127152fc1c9d468
derMacon <silash169@gmail.com>
2019-09-05 04:52:56 +0200

    simple Rest API

---
+++ first-spring-project/src/main/java/com/silas/firstspringproject/FirstSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.silas.firstspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringProjectApplication.class, args);
+	}
+
+}
---
+++ first-spring-project/src/test/java/com/silas/firstspringproject/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.silas.firstspringproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstSpringProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
