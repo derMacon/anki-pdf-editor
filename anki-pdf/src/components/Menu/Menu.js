97feded66edc874d7ffeec9efe4ca7bc7f4867a0
derMacon <silash169@gmail.com>
2019-09-09 01:22:16 +0200

    styled components

--- anki-pdf/src/components/Menu/Menu.js
+++ anki-pdf/src/components/Menu/Menu.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import './Menu.css';
 
 export class Menu extends React.Component {
+  extractFileName(wholePath) {
+    // todo check if correct
+    return wholePath.replace(/\\$/,'').split('\\').pop();
+  }
+
+  openNewPdf() {
+    // todo
+  }
+
+  openNewProject() {
+    // todo
+  }
+
   render() {
-    return <div>{this.props.document.currPage}</div>;
+    const doc = this.props.document;
+    return (
+      <div className="Menu">
+        <button onclick={this.openNewPdf}>Open new pdf</button>
+        <button onclick={this.openNewProject}>Open new project</button>
+        <div className="infoItem">
+          Projectname: todo not in props at the moment
+        </div>
+        <div className="infoItem">
+          Filename: {this.extractFileName(doc.file)}
+        </div>
+        <div className="infoItem">
+          {doc.currPage} / {doc.pageCnt}
+        </div>
+      </div>
+    );
   }
 }
