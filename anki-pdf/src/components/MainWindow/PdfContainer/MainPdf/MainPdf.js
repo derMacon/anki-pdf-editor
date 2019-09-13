d5f90fc412e1421eac53eac8ac453fe4ef17427d
derMacon <silash169@gmail.com>
2019-09-13 15:14:46 +0200

    Added notes

--- anki-pdf/src/components/MainWindow/PdfContainer/MainPdf/MainPdf.js
+++ anki-pdf/src/components/MainWindow/PdfContainer/MainPdf/MainPdf.js
@@ -3,6 +3,12 @@ import {Reader} from '../PdfReader/Reader';
 import './MainPdf.css';
 
 export class MainPdf extends React.Component {
+
+  // should work to read the meta data to determine the current page idx of the
+  // iframe, but it doesn't because js sucks...
+  // const cb = () => {console.log(document.getElementById("mainPdf").contentWindow.PDFViewerApplication)};
+
+
   render() {
     const properties = '?#zoom=0&scrollbar=0&toolbar=0';
     console.log('main: ' + this.props.ApiConnector.getCurrPage_url());
