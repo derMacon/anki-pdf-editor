5a7de1554d712e7995cc08abdf08f3eca2c211d7
derMacon <silash169@gmail.com>
2019-09-16 22:13:25 +0200

    Able to set textarea input via button click

--- anki-pdf/src/util/ApiConnector.js
+++ anki-pdf/src/util/ApiConnector.js
@@ -29,15 +29,6 @@ function requestPost(postSyntax, url) {
 }
 
 function requestGet(url) {
-    // var xmlHttp = new XMLHttpRequest();
-    // xmlHttp.onreadystatechange = function() {
-    //     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-    //         return xmlHttp.responseText;
-    //     }
-    // }
-    // xmlHttp.open("GET", url, true); // true for asynchronous
-    // xmlHttp.send(null);
-
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", url, false ); // false for synchronous request
     xmlHttp.send( null );
