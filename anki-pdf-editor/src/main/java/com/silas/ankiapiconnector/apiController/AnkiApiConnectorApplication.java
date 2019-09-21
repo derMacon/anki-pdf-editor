ab5eebe6bd2c9ee40c65858815ea68a9612adbe6
derMacon <silash169@gmail.com>
2019-09-22 00:40:52 +0200

    Merged project can now load everything from within itself

--- anki-pdf-editor/src/main/java/com/silas/ankiapiconnector/apiController/AnkiApiConnectorApplication.java
+++ anki-pdf-editor/src/main/java/com/silas/ankiapiconnector/apiController/AnkiApiConnectorApplication.java
@@ -3,11 +3,34 @@ package com.silas.ankiapiconnector.apiController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class AnkiApiConnectorApplication {
 
-	public static void main(String[] args) {
+	private static final int API_PORT = 8080;
+	private static final int GUI_PORT = 3000;
+	private static final String KILL_PROCESS_TEMPLATE = "kill $(lsof -t -i:%d)";
+
+
+	public static void main(String[] args) throws IOException {
+		killPorts();
+		startAnki();
 	    SpringApplication.run(AnkiApiConnectorApplication.class, args);
+	    startGui();
+	}
+
+	private static void killPorts() throws IOException {
+		Runtime.getRuntime().exec(String.format(KILL_PROCESS_TEMPLATE, API_PORT));
+		Runtime.getRuntime().exec(String.format(KILL_PROCESS_TEMPLATE, GUI_PORT));
+	}
+
+	private static void startAnki() throws IOException {
+		Runtime.getRuntime().exec("anki");
+	}
+
+	private static void startGui() throws IOException {
+		Runtime.getRuntime().exec("npm start --prefix src/main/js/frontend/");
 	}
 
 }
