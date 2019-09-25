788c0c50dd886ff347b7cece033a057c9b595409
derMacon <silash169@gmail.com>
2019-09-25 04:32:09 +0200

    Build process not working, temp file path wrong

--- project-launcher/src/main/java/com/silas/projectlauncher/Main.java
+++
@@ -1,9 +0,0 @@
-package com.silas.projectlauncher;
-
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        new Launcher().runAll();
-    }
-}
