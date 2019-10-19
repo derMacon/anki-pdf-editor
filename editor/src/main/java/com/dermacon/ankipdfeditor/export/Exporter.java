f077e0709720cfda92ac63d5d661cea62100bdf4
derMacon <silash169@gmail.com>
2019-10-19 22:42:05 +0200

    started implementing img copying process for html generation

--- editor/src/main/java/com/dermacon/ankipdfeditor/export/Exporter.java
+++ editor/src/main/java/com/dermacon/ankipdfeditor/export/Exporter.java
@@ -2,33 +2,72 @@ package com.dermacon.ankipdfeditor.export;
 
 import com.dermacon.ankipdfeditor.data.card.Card;
 import com.dermacon.ankipdfeditor.data.project.AnkiConnector;
+import com.dermacon.ankipdfeditor.data.project.ProjectInfo;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 
 public abstract class Exporter {
 
-    private final String rootDir;
+    private final ProjectInfo projectInfo;
+    protected List<Card> stack;
 
-    public Exporter(String rootDir) {
-        this.rootDir = rootDir;
+    public Exporter(ProjectInfo projectInfo) {
+        this.projectInfo = projectInfo;
     }
 
-    public void createOutput(String deckname) throws IOException {
-        List<Card> stack = AnkiConnector.getCardsFromDeck(deckname);
-        String content = parseFormat(stack);
+    public void createOutput() throws IOException {
+        String deckname = removeExtension(projectInfo.getDeckFile().getName());
+        stack = AnkiConnector.getCardsFromDeck(deckname);
+        copyImages();
+        String content = parseFormat();
         writeFile(deckname, content);
     }
 
-    abstract String parseFormat(List<Card> stack);
+    abstract String parseFormat();
 
     private void writeFile(String deckname, String output) throws IOException {
         System.out.println("todo file io:\n" + output);
-        System.out.println("path: " + rootDir + deckname + ".html");
+        System.out.println("path: " + projectInfo.getExportDir() + deckname + ".html");
 
-        File file = new File(rootDir + deckname + ".html");
+        File file = new File(projectInfo.getExportDir() + deckname + ".html");
         FileUtils.writeStringToFile(file, output, true);
     }
+
+    private void copyImages() {
+        List<String> foundImages = new LinkedList<>();
+
+        for (Card card : stack) {
+            foundImages.addAll(extractImg(card.getFrontSide()));
+            foundImages.addAll(extractImg(card.getBackSide()));
+        }
+
+        for (String img : foundImages) {
+            copyImg(img);
+        }
+    }
+
+    private List<String> extractImg(String sideContent) {
+        // todo
+        System.out.println("extract img - WIP - Exporter.java");
+        return null;
+    }
+
+    private void copyImg(String imgName) {
+        // todo
+        System.out.println("copy img: " + imgName);
+    }
+
+    // todo duplicate from request
+    protected static String removeExtension(String fullFileName) {
+        int idx = fullFileName.lastIndexOf('.');
+        if (idx > 0) {
+            fullFileName = fullFileName.substring(0, idx);
+        }
+        return fullFileName;
+    }
+
 }
